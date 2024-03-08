389a2e7b0e98ceef1dd6939eae1388f2b1b82d73
HaiPham <pthai076@icloud.com>
2024-03-08 08:44:52 +0700

    feat: update code

--- company/show-web-dialog/ContentView.swift
+++ company/show-web-dialog/ContentView.swift
@@ -11,7 +11,10 @@ struct ContentView: View {
 	@State var presentingModal = false
 	
 	var body: some View {
-		Text("Hello, world!")
+		Image("sample")
+			.resizable()
+			.scaledToFit()
+		Text("Figure 3: Illustration of how treatment bias in training dataset leads to wrong incentive decisions. There are three types of users: active (red), ordinary (blue), and inactive (green). Each type has higher response rate to higher incentive (cost). However, in collected data, user activity is negatively correlated with incentive. A DNN model without knowledge of this bias will follow the dotted line, which does not reflect the causal effect of incentive. Based on this DNN model, a decision engine will never use any incentive larger than the wrong best cost")
 			.padding()
 		
 		Button("Show dialog") {
